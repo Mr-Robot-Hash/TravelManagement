e179f16b31fb2fa8a804b9a9f5925215a7af4b5e
Mr.Robot <harshyadav3896@gmail.com>
2020-02-15 22:03:42 +0530

    Changes for HelloWorld2

--- src/main/java/com/project/TravelManagementApplication.java
+++ src/main/java/com/project/TravelManagementApplication.java
@@ -8,6 +8,8 @@ public class TravelManagementApplication {
 
 	public static void main(String[] args) {
 		System.out.println("Hello World");
+		System.out.println("Hello World2");
+		System.out.println("Hello World2");
 		SpringApplication.run(TravelManagementApplication.class, args);
 	}
 
