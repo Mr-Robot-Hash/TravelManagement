5a655d3daaea7c8e99b4ce59ce80eda0d9fc3d87
Mr.Robot <harshyadav3896@gmail.com>
2020-02-15 23:18:03 +0530

    Fix Pom

--- src/main/java/com/project/TravelManagementApplication.java
+++ src/main/java/com/project/TravelManagementApplication.java
@@ -7,11 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class TravelManagementApplication {
 
 	public static void main(String[] args) {
-		System.out.println("Hello World");
-		System.out.println("Hello World2");
-		System.out.println("Hello World2");
-		System.out.println("Hello World3");
-		System.out.println("Hello World4");
+		System.out.println("Hello World");		
 		SpringApplication.run(TravelManagementApplication.class, args);
 	}
 
